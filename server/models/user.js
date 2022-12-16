3bd7ecd9bfa6b2aacb0941948898591e76470a39
anandukch <anandukch@gmail.com>
2022-12-17 01:41:24 +0530

    server setup

--- server/models/user.js
+++ server/models/user.js
@@ -26,13 +26,6 @@ const userSchema = new mongoose.Schema({
 // userSchema.pre("save",()=>{
 
 // })
-userSchema.methods.createJWT = async function () {
-  const user = this;
-  const token = jwt.sign({ id: user._id.toString(), userType: "normal" }, process.env.JWT_SECRET);
-  user.tokens = user.tokens.concat({ token });
-  await user.save();
-  return token;
-};
 const UserModel = mongoose.model("User", userSchema);
 module.exports = UserModel;
 
