10352b0f312b73cec6b7128ebed658de80b6f104
sreerag <msratofficial@gmail.com>
2022-12-17 02:04:54 +0530

    feat:create docter schema

---
+++ server/models/docter.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+
+const docterSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    trim: true,
+    lowercase: true,
+    unique: true,
+    required: true,
+  },
+  firstName: {
+    type: String
+  },
+  lastName: {
+    type: String
+  },
+  phno: {
+    type: String
+  },
+  timeAvailible:{
+    type:Date
+  }
+});
+
+// docterSchema.pre("save",()=>{
+
+// })
+const DocterModel = mongoose.model("User", docterSchema);
+module.exports = DocterModel;
+
