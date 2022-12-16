2661ba9b17d85a63d1f1aa5cb8b693afe423e2ee
anandukch <anandukch@gmail.com>
2022-12-16 23:05:35 +0530

    basic server code pushed

---
+++ server/config/db.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const connectDB = () => {
+    // console.log(process.env.MONGOURL_PROD);
+    mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }).then(() => {
+      console.log('MongoDB Connected...');
+    }
+    ).catch(err => {
+      console.log(err);
+    }
+    );
+  }
+  module.exports = connectDB;
