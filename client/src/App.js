cff0d6fbb8d92d3891d488e3445bae29108ee8b8
alvin1904 <alvin19official@gmail.com>
2022-12-17 01:11:24 +0530

    add book_doctor page

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import NavBar from "./components/user/Navbar";
 import SignIn from "./components/user/Signin";
 import SignUp from "./components/user/Signup";
 import HomeMain from "./components/user/dashboard/HomeMain";
+import BookDoctor from "./components/user/book_doctor/BookDoctor";
 
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
@@ -13,6 +14,7 @@ function App() {
         <Route path="/signin" element={<SignIn />} />
         <Route path="/register" element={<SignUp />} />
         <Route path="/home" element={<HomeMain />} />
+        <Route path="/book" element={<BookDoctor />} />
       </Routes>
     </Router>
   );
