5c52671528ba2976043f96210a1d16aecc9ded0e
sreerag <msratofficial@gmail.com>
2022-12-17 10:06:43 +0530

    feat:create create appointment route

---
+++ server/controllers/user/appointment.js
@@ -0,0 +1,22 @@
+const appointmentModel = require("../../models/doctor");
+const { StatusCodes, getReasonPhrase } = require('http-status-codes');
+const CustomError = require("../../error/custom");
+const asyncWrapper = require("../../error/asyncWrapper");
+
+const createAppointment = asyncWrapper(async (req, res) => { //auth with normal token
+    const appointment = await appointmentModel.create(req.body);
+    if (!appointmentData) throw new CustomError("data not present", StatusCodes.BAD_REQUEST);
+    res.status(StatusCodes.OK).json({
+        "timeSlot": appointmentData.timeSlot,
+        "doctorId": appointmentData.doctorId,
+        "patientId": appointmentData.patientId,
+    })
+
+})
+
+
+
+
+module.exports = {
+    createAppointment
+}
