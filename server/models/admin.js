85266a5f1e28bcca38845feb9a7aa79c0a4f21d6
sreerag <msratofficial@gmail.com>
2022-12-17 09:05:12 +0530

    feat:create auth routes for admin

---
+++ server/models/admin.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+
+const adminSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    trim: true,
+    lowercase: true,
+    unique: true,
+    required: true,
+  },
+  userName:{
+    type:String
+  },
+  password: {
+    type: String
+  }
+});
+
+// adminSchema.pre("save",()=>{
+
+// })
+const AdminModel = mongoose.model("User", adminSchema);
+module.exports = AdminModel;
+
