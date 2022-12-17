7e537f28a6d5c542469c759da8944a00b3e5bef9
anandukch <anandukch@gmail.com>
2022-12-17 10:15:35 +0530

    date adde din time slot schema

--- server/models/timeSlotAnalysis.js
+++ server/models/timeSlotAnalysis.js
@@ -2,6 +2,9 @@ const mongoose = require("mongoose");
 const jwt = require("jsonwebtoken");
 
 const timeSlotAnalysisSchema = new mongoose.Schema({
+  appointmentDate: {
+    type: Date
+  },
   doctorId: {
     type: mongoose.Types.ObjectId
   },
@@ -13,9 +16,6 @@ const timeSlotAnalysisSchema = new mongoose.Schema({
   }
 });
 
-// userSchema.pre("save",()=>{
-
-// })
 const TimeSlotAnalysisModel = mongoose.model("timeSlotAnalysis", timeSlotAnalysisSchema);
 module.exports = TimeSlotAnalysisModel;
 
