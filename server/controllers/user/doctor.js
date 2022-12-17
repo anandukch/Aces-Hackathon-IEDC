e92f3f9bcde0da2978db324966419095cdb693ac
anandukch <anandukch@gmail.com>
2022-12-17 11:25:54 +0530

    updated

--- server/controllers/user/doctor.js
+++ server/controllers/user/doctor.js
@@ -7,16 +7,16 @@ const getDoctors = asyncWrapper(async (req, res) => { //auth with normal token
     const doctorData = await DoctorModel.find();
     if (!doctorData) throw new CustomError("data not present", StatusCodes.BAD_REQUEST);
     res.status(StatusCodes.OK).json({
-        "firstName": doctorData.firstName,
-        "lastName": doctorData.lastName,
-        "timeAvailable": doctorData.timeAvailable,
-        "email": doctorData.email,
-        "phoneNumber": doctorData.phno
+        firstName: doctorData.firstName,
+        lastName: doctorData.lastName,
+        timeAvailable: doctorData.timeAvailable,
+        email: doctorData.email,
+        phoneNumber: doctorData.phno
     })
 
 })
 
- 
+
 const getTime = asyncWrapper(async (req, res) => {
     const { id } = req.params;
     if (!id) throw new CustomError("no data present", StatusCodes.BAD_REQUEST);
